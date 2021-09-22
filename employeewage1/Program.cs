294d111de87f0919447b262791dff19bc5ffc8bb
varnikak00 <varnikak7070@gmail.com>
2021-09-22 09:59:55 +0530

    emp1

---
+++ employeewage1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace EmployeeWage1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Employee = 1;
+            int EmployeeWage = 1;
+            if (Employee == 1)
+            {
+                Console.WriteLine("Employee is present");
+                Employee = 238 * 12;
+                Console.WriteLine("Employee Wage =" + EmployeeWage);
+            }
+            else
+                Console.WriteLine("Employee is absent");
+            Console.WriteLine("No wage is calculated");
+        }
+    }
+}
+
