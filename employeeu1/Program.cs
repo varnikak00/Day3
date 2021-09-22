44a76faa5c19710c0f548ff9e2e16f359aef66d5
varnikak00 <varnikak7070@gmail.com>
2021-09-22 10:14:56 +0530

    emp2

---
+++ employeeu1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Employee1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Employee1 = 0;
+            if (Employee1 == 1)
+                Console.WriteLine("Employee present");
+            else
+                Console.WriteLine("Employee absent");
+        }
+    }
+}
